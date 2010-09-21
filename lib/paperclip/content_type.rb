5f6a6cc422faa6ca7a4420dcbcd0c1d8a72584fe
Claudio Poli <masterkain@gmail.com>
2010-09-21 04:27:22 +0200

    more

--- lib/paperclip/content_type.rb
+++ lib/paperclip/content_type.rb
@@ -28,6 +28,18 @@ module Paperclip
           when "txt"                     then "text/plain"
           when %r"html?"                 then "text/html"
           when "csv", "xml", "css", "js" then "text/#{type}"
+          when "aac" then "audio/aac"
+          when "mp3", "mp2" then "audio/mpeg"
+          when "m4a", "m4b", "m4p", "m4v", "m4r", "3gp", "mp4" then "audio/mp4"
+          when "ogg" then "application/ogg"
+          when "flac" then "audio/flac"
+          when "spx" then "audio/speex"
+          when "wma" then "audio/x-ms-wma"
+          when "rm", "ram" then "audio/x-pn-realaudio"
+          when "wav" then "audio/vnd.wave"
+          when "mpc", "mp+", "mpp" then "audio/x-musepack"
+          when "aiff", "aif", "aifc" then "audio/x-aiff"
+          when "tta" then "audio/x-tta"
           else "application/x-#{type}"
         end
       end
